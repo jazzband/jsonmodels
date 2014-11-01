0d10bd43efe9e29351b81a443ce636b6cd64e190
Szczepan Cieślik <szczepan.cieslik@gmail.com>
2014-11-01 10:04:48 +0100

    Moved to PyTest.

--- setup.py
+++ setup.py
@@ -5,17 +5,33 @@
 import os
 import sys
 
+from setuptools.command.test import test as TestCommand
 from jsonmodels import __version__ as version
 
+from setuptools import setup
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 # Hacking unittest.
 try:
@@ -64,5 +80,10 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
-    test_suite='tests',
+    tests_require=[
+        'pytest',
+    ],
+    cmdclass = {
+        'test': PyTest,
+    },
 )

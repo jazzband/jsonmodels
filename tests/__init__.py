9a9cc6e7f990730958675d039a40ed7bd3c0eeda
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2016-12-29 10:08:03 +0200

    Exclude flake8 from 2.7, 3.2, 3.3 builds
    flake8 no longer supports python 2.6,3.2,3.3

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,10 @@
-LINT = True
+def _have_flake8():
+    try:
+        import flake8  # noqa: F401
+        return True
+    except ImportError:
+        return False
+
+
+LINT = _have_flake8()
 CHECK_SPELLING = False
